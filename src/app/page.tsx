bac8f6cd2c80f9215966365054a837d5f5f30a10
Egidio Caprino <egidio.caprino@gmail.com>
2023-07-05 12:27:43 +0200

    feat: add Support and Github links in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Converter from "@/components/Converter";
+import { Fragment } from "react";
 
 const HomePage = () => (
-  <main className="flex flex-col items-center content-center space-y-8 p-24 font-sans">
-    <h1 className="text-xl">From Invidious to NewPipe</h1>
-    <Converter />
-  </main>
+  <Fragment>
+    <header className="flex justify-end gap-8 p-8 text-slate-500">
+      <a href="https://github.com/EgidioCaprino/newpipe-subscriptions/issues/new">
+        Support
+      </a>
+      <a href="https://github.com/EgidioCaprino/newpipe-subscriptions">
+        Github
+      </a>
+    </header>
+    <main className="flex flex-col items-center content-center space-y-8 p-24 font-sans">
+      <h1 className="text-xl">From Invidious to NewPipe</h1>
+      <Converter />
+    </main>
+  </Fragment>
 );
 
 export default HomePage;
